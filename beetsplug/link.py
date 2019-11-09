228310e3bfd34056227ab02ed07d075702f36ad0
ybnd <ybnd@tuta.io>
2019-11-09 20:45:55 +0100

    Add placeholders for internal methods

--- beetsplug/link.py
+++ beetsplug/link.py
@@ -30,10 +30,10 @@ class LinkPlugin(BeetsPlugin):
     def __init__(self):
         super(LinkPlugin, self).__init__()
 
-        self.register_listener('album_imported', self.infer)
+        self.register_listener('album_imported', self.on_import)
         # https://beets.readthedocs.io/en/v1.3.17/dev/plugins.html#listen-for-events
 
-    def infer(self, lib, album):
+    def on_import(self, lib, album):
         """ Check for split/compilation 'flags':
                 - comp:T?
                     * maybe: separate comp into comp and split, add option for additional fields
@@ -61,9 +61,31 @@ class LinkPlugin(BeetsPlugin):
         print(f'Should be removing links from library')
         pass
 
-    def _link_split(self):
-        """ Add symlinks to split to all relevant albumartist directories; create if not already present
-        """
+    def _check_all_albums(self, 'lib', 'album'):
+        """ For all albums in library, check whether they could be a split (will be slow, probably) """
+        pass
+
+    def _get_all_splits(self, lib):
+        pass
+
+    def _infer_if_split(self, lib, album):
+        """ Infer whether album is a split based on config """
+        pass
+
+    def _prompt_user(self):
+        """ Prompt user whether it's okay to consider this album a split, and whether the list of collaborators is ok """
+        pass
+
+    def _write_collaborators(self, lib, album):
+        """ Save list of collaborators to album metadata """
+        pass
+
+    def _add_links(self, lib, album):
+        """ For every collaborator in album, add a link in their respective directories if they meet the conditions set in config """
+        pass
+
+    def _remove_links(self, lib, album):
+        """ For every collaborator in album, remove link if present """
         pass
 
     def commands(self):
