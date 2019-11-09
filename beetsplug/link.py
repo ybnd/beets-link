90a378b531b40decacffbbbc90d7807e8f721e4a
ybnd <ybnd@tuta.io>
2019-11-09 20:35:47 +0100

    Get on-import and CLI method calls working

--- beetsplug/link.py
+++ beetsplug/link.py
@@ -7,22 +7,33 @@ from beets.util import mkdirall, normpath, syspath
 import beets.autotag.hooks as hooks
 import os
 
-class SplitsPlugin(BeetsPlugin):
+import os.path
+import threading
+import argparse
+from concurrent import futures
+import six
+
+import beets
+from beets import util, art
+from beets.plugins import BeetsPlugin, log
+from beets.ui import Subcommand, get_path_formats, input_yn, UserError, print_
+from beets.library import parse_query_string, Item
+from beets.util import syspath, displayable_path, cpu_count, bytestring_path
+
+from beetsplug import convert
+
+class LinkPlugin(BeetsPlugin):
     """
 
     """
 
     def __init__(self):
-        super(SplitsPlugin).__init__()
+        super(LinkPlugin, self).__init__()
 
-        self.config.add({
-
-        })
-
-        self.register_listener('album_imported', self.infer_split)
+        self.register_listener('album_imported', self.infer)
         # https://beets.readthedocs.io/en/v1.3.17/dev/plugins.html#listen-for-events
 
-    def infer_split(self, lib, album):
+    def infer(self, lib, album):
         """ Check for split/compilation 'flags':
                 - comp:T?
                     * maybe: separate comp into comp and split, add option for additional fields
@@ -38,6 +49,16 @@ class SplitsPlugin(BeetsPlugin):
         """
         # https://beets.readthedocs.io/en/v1.3.17/dev/api.html#the-library-class
         # https://beets.readthedocs.io/en/v1.3.17/dev/api.html#album
+
+        print(f"Got here!")
+        pass
+
+    def add(self, lib, opts):
+        print(f'Should be adding links to library ~ config')
+        pass
+
+    def remove(self, lib, opts):
+        print(f'Should be removing links from library')
         pass
 
     def _link_split(self):
@@ -46,12 +67,28 @@ class SplitsPlugin(BeetsPlugin):
         pass
 
     def commands(self):
-        return [SplitsCommand(self)]
+        return [LinkCommand(self)]
 
 
-class SplitsCommand(Subcommand):
-    name = 'splits'
-    help = "add links to split and compilation albums to the contributing artists' directories"
+class LinkCommand(Subcommand):
+    name = 'link'
+    help = 'add links to split and compilation albums to the contributing artists\' directories'
 
     def __init__(self, plugin):
-        super(SplitsCommand, self).__init__(self.name, self.help)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--add', action='store_const', dest='action', const='add')
+        parser.add_argument('--remove', action='store_const', dest='action', const='remove')
+
+        self.parser = parser
+        self.plugin = plugin
+
+        super(LinkCommand, self).__init__(self.name, parser, self.help)
+
+    def func(self, lib, opts, _):
+        if opts.action == 'add':
+            self.plugin.add(lib, opts)
+        elif opts.action == 'remove':
+            self.plugin.remove(lib, opts)
+
+    def parse_args(self, args):
+        return self.parser.parse_args(args), []
